9d265cc9c0960ada8fccef312261ccd34ae86f74
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2023-02-13 22:28:08 +0100

    Addition to training

--- Python/ToDo/Exercices/Exercise4.py
+++ Python/ToDo/Exercices/Exercise4.py
@@ -2,3 +2,11 @@
 dollars = int(input("Please enter how many dollars to convert: "))
 print("You would get " + str(dollars * 0.95) + " Euros...")
 
+# Drivers ranking
+ranking = ["Ken Block", "Travis Pastrana", "Colin McRae"]
+position = int(input("Which place do U want to know?: "))
+print(ranking[position - 1])
+
+# Search in above list (ranking) by name
+name = input("Enter driver´s name to show position: ")
+print(ranking.index(name) + 1)
