5be907abc2b4f750f2c7fe65c9f8604fc5c3cea0
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2023-02-13 22:47:25 +0100

    Addition to training

--- Python/ToDo/main.py
+++ Python/ToDo/main.py
@@ -9,23 +9,21 @@ while True:
     match user_action:
         case 'add' | 'a':
             todo = input("What do U want to add: ")
-            todos.append(todo)
+            todos.append(todo)  # adds to the end of list
         case 'show' | 'display' | 's':
-            i = 1
-            for each in todos:
-                print(str(i) + ') ' + each.capitalize())
-                i = i + 1
+            for i, each in enumerate(todos):  # enumerate returns index(i) of element + the list element in ()
+                print(f"{i + 1}) {each}")  # fstring - helps writing exactly variables in a string in single quotes
         case 'edit' | 'e':
             number = int(input("Number of ToDo to Edit: "))
-            number = number - 1
+            number = number - 1  # because we want to start from 1 and not zero
             new_todo = input("What will be the new ToDo?: ")
             todos[number] = new_todo
         case 'delete' | 'd':
             number = int(input("Number of which one to delete?: "))
             number = number - 1
-            todos.pop(number)
+            todos.pop(number)  # deletes and returns element
         case 'quit' | 'q':
-            break
+            break  # stops the while TRUE loop
         case _:
             print("Not a valid command")
 print("Bye and have a nice day!")
