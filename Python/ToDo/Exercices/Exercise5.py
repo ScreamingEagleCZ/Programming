46598147d2da04e91a03282276af2cefd5e32692
Jakub Mukařovský <jakub.mukarovsky@gmail.com>
2023-02-15 23:40:23 +0100

    Addition to training

---
+++ Python/ToDo/Exercices/Exercise5.py
@@ -0,0 +1,8 @@
+filenames = ['document', 'report', 'presentation']
+for i, j in enumerate(filenames):
+    print(f"{i}-{j.capitalize()}.txt")
+
+ips = ['100.122.133.105', '100.122.133.111']
+ip = int(input("Enter the Index of IP u want to show: "))
+print(f" You have chosen {ips[ip]}")
+
--- Python/ToDo/main.py
+++ Python/ToDo/main.py
@@ -1,37 +1,58 @@
 print("To-Do list")
 
-user_prompt = "Type Add, Show, Edit, Complete, Delete or Quit: "
-todos = []
+user_prompt = "Type Add, Show, Edit, Complete or Quit: "
+# todos = [] - unusable with file handling
 
 while True:
     user_action = input(user_prompt)
     user_action = user_action.strip()  # strip cuts blank places from string
     match user_action:
         case 'add' | 'a':
-            todo = input("What do U want to add: ")
+            todo = input("What do U want to add: ") + "\n"  # adds break line to input (and to todos.txt)
+
+            file = open('todos.txt', 'r')  # opens file with read privileges
+            todos = file.readlines()  # reads the data from file
+            file.close()  # close the file after no longer needed
+
             todos.append(todo)  # adds to the end of list
+
+            file = open('todos.txt', 'w')  # opens file with Write privileges
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display' | 's':
+            file = open('todos.txt', 'r')  # opens file for reading
+            todos = file.readlines()
+            file.close()
             for i, each in enumerate(todos):  # enumerate returns index(i) of element + the list element in ()
                 print(f"{i + 1}) {each}")  # fstring - helps writing exactly variables in a string in single quotes
 
         case 'edit' | 'e':
+            file = open('todos.txt', 'r')  # opens file for reading
+            todos = file.readlines()
+            file.close()
+
             number = int(input("Number of ToDo to Edit: "))
             number = number - 1  # because we want to start from 1 and not zero
-            new_todo = input("What will be the new ToDo?: ")
+            new_todo = input("What will be the new ToDo?: ") + "\n"
             todos[number] = new_todo
-        case 'delete' | 'd':
-            number = int(input("Number of which one to delete?: "))
-            number = number - 1
-            todos.pop(number)  # deletes and returns element
-        case 'complete' | 'c':
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+        case 'complete' | 'c':  # technically delete
+            file = open("todos.txt", 'r')
+            todos = file.readlines()
+            file.close()
+
             number = int(input("Number of which one to complete?: "))
             number = number - 1
             todos.pop(number)  # deletes and returns element
+
+            file = open("todos.txt", 'w')
+            file.writelines(todos)
+            file.close()
         case 'quit' | 'q':
             break  # stops the while TRUE loop
         case _:
             print("Not a valid command")
 print("Bye and have a nice day!")
-
-
-
