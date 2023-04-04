310f33abd7e83fc6230d672b69eda909628ff81e
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-04-04 16:26:51 +0200

    Update buttons

--- Python/ToDo/Exercices/Exercise17_18_gui_feet_inches.py
+++ Python/ToDo/Exercices/Exercise17_18_gui_feet_inches.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as Sg
 
+Sg.theme("Black")
+
 
 def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
@@ -11,12 +13,13 @@ label_inches = Sg.Text("Enter inches: ")
 input_feet = Sg.Input()
 input_inches = Sg.Input()
 button = Sg.Button("Convert", key="convert")
+button_exit = Sg.Button("Exit")
 output_label = Sg.Text("", key="meters")
 
 window = Sg.Window("Feet & Inches to meters converter",
                    [[label_feet, input_feet],
                     [label_inches, input_inches],
-                    [button, output_label]])
+                    [button, button_exit, output_label]])
 
 
 while True:
@@ -24,9 +27,14 @@ while True:
     print(event)
     match event:
         case "convert":
-            print(values)
-            result = convert(float(values[0]), float(values[1]))
-            output_label.update(f"{result} m")
+            try:
+                print(values)
+                result = convert(float(values[0]), float(values[1]))
+                output_label.update(f"{result} m")
+            except ValueError:
+                Sg.Popup("You are supposed to enter 2 numbers")
+        case "Exit":
+            break
         case Sg.WIN_CLOSED:
             break
 
