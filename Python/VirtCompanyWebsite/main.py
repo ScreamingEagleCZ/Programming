6b0df38c9547a4c33c71a20a39424b47e3bbaed3
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-14 15:51:58 +0200

    D23: Add Navigation menu and contact us page

--- Python/VirtCompanyWebsite/main.py
+++ Python/VirtCompanyWebsite/main.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-st.title("ACME Corporation")
+st.header("ACME Corporation")
 content = """
 WNNA ANKLNLANF KALNFLKKNAKak nla knalfnal n klanlfkn alknsf lkan lsknflak f 
 alkkn alfalksf al lklklaksf lkanslfn lakngllewioo lm lknlkangkangaga
@@ -11,16 +11,14 @@ alsfjbui olainglla lkanflan lknalfnalnfliianf agag.
 """
 st.info(content)
 
-st.title("Our Team")
+st.subheader("Our Team")
 col1, empty_col, col2, empty_col2, col3 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=",")
 
 with col1:
     for index, row in df[:4].iterrows():
-        first = row['first name'].capitalize()
-        last = row['last name'].capitalize()
-        st.header(f"{first} {last}")
+        st.header(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"])
 
