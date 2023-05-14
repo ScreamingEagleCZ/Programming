e903e00d889bb6d993eda46b118e3e93aad193f6
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-14 17:33:27 +0200

    Virtual Company - form sent via email added csv file for topics

--- Python/VirtCompanyWebsite/pages/Contact_Us.py
+++ Python/VirtCompanyWebsite/pages/Contact_Us.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
+df = pandas.read_csv("topics.csv")
 st.header("Contact Us")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email address:")
     option = st.selectbox(
         "What topic do You want to discuss?",
-        ('Job inquiries', 'Project proposals', 'Others'))
-    st.write("You selected:", option)
+        df["topic"])
+    # st.write("You selected:", option)
     raw_message = st.text_area("Your message here:")
     message = f"""\
 Subject: New Email from: {user_email}
