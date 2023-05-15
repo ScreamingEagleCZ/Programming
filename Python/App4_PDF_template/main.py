2941f34bf76edee0a619193597806c617ee98ce0
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-15 15:39:41 +0200

    D24: PDFCreation - Added lines to pages

--- Python/App4_PDF_template/main.py
+++ Python/App4_PDF_template/main.py
@@ -16,7 +16,12 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(x1=10, y1=21, x2=80, y2=21)
+    pdf.line(x1=10, y1=20, x2=80, y2=20)
+
+    # lines on master page
+    for i in range(30, 290, 10):
+        pdf.line(x1=10, y1=i, x2=200, y2=i)
+
     # footer for master page
     pdf.ln(260)
     pdf.set_font(family="Times", style="I", size=10)
@@ -24,6 +29,11 @@ for index, row in df.iterrows():
     actual = actual + 1
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # lines on auxiliary pages
+        for i in range(10, 290, 10):
+            pdf.line(x1=10, y1=i, x2=200, y2=i)
+
         # footer for auxiliary pages
         pdf.ln(272)
         pdf.set_font(family="Times", style="I", size=10)
