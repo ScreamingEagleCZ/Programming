45676b50c9c67ff6e233091997f832a5b79174a4
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-15 15:00:32 +0200

    D24: PDFCreation - Added pages for topics

--- Python/App4_PDF_template/main.py
+++ Python/App4_PDF_template/main.py
@@ -6,15 +6,13 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    pages = row["Pages"]
-    i = 1
-    while i <= pages:
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(x1=10, y1=21, x2=80, y2=21)
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.set_font(family="Times", style="B", size=24)
-        pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-        pdf.line(x1=10, y1=21, x2=80, y2=21)
-        i = i + 1
 
 
 pdf.output("output.pdf")
