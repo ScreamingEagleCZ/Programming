fdbb667bf7924c9b4b0fd8bf28635ab3dfd30cc9
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-16 16:01:42 +0200

    D25: PDF invoice - Added date to invoice

--- Python/App5_Excel_Invoice/main.py
+++ Python/App5_Excel_Invoice/main.py
@@ -8,11 +8,21 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    # print(df)
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem  # extracts name of file from filepath
-    invoice_nr = filename.split("-")[0]  # extracts nr from name (only first split from list)
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+    invoice_nr, date = filename.split("-")  # extracts nr from name and date in 2 separate strings from list
+
+    # nr of invoice
+    pdf.set_font(family="Times", size=24, style="B")
+    pdf.cell(w=50, h=12, txt=f"Invoice nr. {invoice_nr}", ln=1)
+
+    # date of invoice
+    pdf.set_font(family="Times", size=20, style="B")
+    pdf.cell(w=50, h=12, txt=f"Date {date}", ln=1)
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
