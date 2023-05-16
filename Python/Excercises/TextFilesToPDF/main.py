bc84bcf97861a97ce4ffa6a4155633680dbabdc3
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-16 16:58:22 +0200

    D25 Excercise: Create PDF from files add text from files

--- Python/Excercises/TextFilesToPDF/main.py
+++ Python/Excercises/TextFilesToPDF/main.py
@@ -12,8 +12,15 @@ for filepath in filepaths:
     filename = Path(filepath).stem  # extracts name
     name = filename.title()
     pdf.set_font(family="Times", size=24, style="B")
-    pdf.cell(w=50, h=12, txt=name)
+    pdf.cell(w=50, h=12, txt=name, ln=1)
 
+    # Add text to files
+    with open(filepath, "r") as file:
+        content = file.read()
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+# output to pdf
 pdf.output("Animals.pdf")
 
 
