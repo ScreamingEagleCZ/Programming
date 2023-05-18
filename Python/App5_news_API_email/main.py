558b3b1355dd09d7f0c277c82ed1633a858fdcc8
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-18 15:52:08 +0200

    D27 - Access News titles and description

---
+++ Python/App5_news_API_email/main.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = "9421379a45cc428eaaef6960f0e5899e"
+url = "https://newsapi.org/v2/everything?q=tesla&" \
+      "from=2023-04-18&sortBy=publishedAt&" \
+      "apiKey=9421379a45cc428eaaef6960f0e5899e"
+
+request = requests.get(url)  # make request
+content = request.json()  # get dictionary with data
+
+print(content["articles"])
+print(type(content))
+
+# access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+
