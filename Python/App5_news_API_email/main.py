8232984b0e94b2ad95a3818117b5f65bcade142b
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-18 16:21:13 +0200

    D27 - Send email with News titles and description

--- Python/App5_news_API_email/main.py
+++ Python/App5_news_API_email/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "9421379a45cc428eaaef6960f0e5899e"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
@@ -8,11 +9,18 @@ url = "https://newsapi.org/v2/everything?q=tesla&" \
 request = requests.get(url)  # make request
 content = request.json()  # get dictionary with data
 
-print(content["articles"])
-print(type(content))
+# print(content["articles"])
+# print(type(content))
+content_to_send = ""
 
 # access the article titles and description
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    # print(article["title"])
+    # print(type(article["title"]))
+    # print(f"{article['title']} - {article['description']}")
+    content_to_send = content_to_send + f"{article['title']} - {article['description']} \n\n"
+    # print(article["description"])
 
+content_to_send = content_to_send.encode("utf-8")
+send_email(content_to_send)
+print(content_to_send)
---
+++ Python/App5_news_API_email/send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import os  # environment variable for hiding password (from win env.variable)
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jakub.mukarovsky@gmail.com"
+    password = os.getenv("PASSWORD")  # hiding pass in environment variable
+    receiver = "jakub.mukarovsky@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+# send_email("test")
