435a6008e84e64b86f4102e79ef1d333f2809d38
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2023-05-19 16:08:18 +0200

    D28 - Add links, specify language, limit news, added subject, control category

--- Python/App5_news_API_email/main.py
+++ Python/App5_news_API_email/main.py
@@ -1,25 +1,34 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "9421379a45cc428eaaef6960f0e5899e"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2023-04-18&sortBy=publishedAt&" \
-      "apiKey=9421379a45cc428eaaef6960f0e5899e"
+
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}s&" \
+      "from=2023-04-19&sortBy=publishedAt&" \
+      "apiKey=9421379a45cc428eaaef6960f0e5899e" \
+      "&language=en"
 
 request = requests.get(url)  # make request
 content = request.json()  # get dictionary with data
 
 # print(content["articles"])
 # print(type(content))
-content_to_send = ""
+content_to_send = "Subject: Today`s news \n"
 
 # access the article titles and description
-for article in content["articles"]:
-    # print(article["title"])
-    # print(type(article["title"]))
-    # print(f"{article['title']} - {article['description']}")
-    content_to_send = content_to_send + f"{article['title']} - {article['description']} \n\n"
-    # print(article["description"])
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        if article["description"] is not None:
+            if article["url"] is not None:
+                # print(article["title"])
+                # print(type(article["title"]))
+                # print(f"{article['title']} - {article['description']}")
+                content_to_send = content_to_send + article["title"] + "\n" \
+                                  + article["description"] + "\n" \
+                                  + article["url"] + 2*"\n"
+                # print(article["description"])
 
 content_to_send = content_to_send.encode("utf-8")
 send_email(content_to_send)
