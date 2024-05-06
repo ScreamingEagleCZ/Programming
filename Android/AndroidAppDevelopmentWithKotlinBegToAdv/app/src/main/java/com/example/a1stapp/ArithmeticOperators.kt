18e1857d14b3648f37f840f24ca73940384f8ed5
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2024-05-06 08:32:17 +0200

    Backup
    Backup

---
+++ Android/AndroidAppDevelopmentWithKotlinBegToAdv/app/src/main/java/com/example/a1stapp/ArithmeticOperators.kt
@@ -0,0 +1,22 @@
+package com.example.a1stapp
+
+fun main() {
+    var number1:Double = 8.4
+    var number2:Double = 4.2
+    var result:Double = 0.0
+
+    result = number1 + number2
+    println("number1 + number2 = " + result)
+
+    result = number1 - number2
+    println("number1 - number2 = " + result)
+
+    result = number1 * number2
+    println("number1 * number2 = " + result)
+
+    result = number1 / number2
+    println("number1 / number2 = " + result)
+
+    result = number1 % number2
+    println("number1 % number2 = " + result)
+}
