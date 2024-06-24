e2ae868b45b572d6b1ad40f7533925767d5aac64
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2024-06-24 17:07:19 +0200

    Initial commit
    Continuation of course...

---
+++ C# Course/C_Course_Chapter10_MathClass/C_Course_Chapter10_MathClass/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace C_Course_Chapter10_MathClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ceiling: " + Math.Ceiling(15.5));
+            Console.WriteLine("Floor: " + Math.Floor(15.9));
+
+            int num1 = 13;
+            int num2 = 5;
+            Console.WriteLine("Lower of num1 {0} and num2 {1} is {2}", num1, num2, Math.Min(num1, num2));
+            Console.WriteLine("Higher of num1 {0} and num2 {1} is {2}", num1, num2, Math.Max(num1, num2));
+            Console.WriteLine("2 to the power of 8 is {0}", Math.Pow(2, 8));
+            Console.WriteLine("Pi is {0}", Math.PI);
+            Console.WriteLine("Square root of 25 is {0}", Math.Sqrt(25));
+            Console.WriteLine("Always positive is {0}", Math.Abs(-654321));
+            Console.WriteLine("Cos of 1 is {0}", Math.Cos(1));
+            Run("10");
+            Run("90");
+            Run("180");
+            Console.ReadLine();
+
+            static void Run(string line)
+            {
+                int angle;
+
+                if (Int32.TryParse(line, out angle) == false || (angle < 0 || angle > 180))
+                {
+                    System.Console.WriteLine("Check the input!");
+                    return;
+                }
+
+                System.Console.WriteLine("Cos = {0}", Math.Cos(ConvertToRadians(angle)));
+                System.Console.WriteLine("Sin = {0}", Math.Sin(ConvertToRadians(angle)));
+                System.Console.WriteLine("Tg = {0}", Math.Tan(ConvertToRadians(angle)));
+            }
+
+            static double ConvertToRadians(double angle)
+            {
+                return (Math.PI / 180) * angle;
+            }
+        }
+    }
+}
---
+++ C# Course/C_Course_Chapter10_Random/C_Course_Chapter10_Random/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace C_Course_Chapter10_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the famous fortune teller Jebbedyah!!!");
+            Console.WriteLine("What is your question? But I will anser only yes, no or maybe!");
+            Console.ReadLine();
+            Random yesNoMaybe = new Random();
+            int question = yesNoMaybe.Next(1, 4);   // Random nr generator 1-3 (4th is excluded)
+            if (question == 0) { Console.WriteLine("Yes OFC!"); }
+            else if (question == 1) { Console.WriteLine("Maybe babe..."); }
+            else { Console.WriteLine("Definitelly not!"); }
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
