480f3bf164fb517fcca949cdd5e5cbf10c3c3cc7
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2024-07-02 14:48:56 +0200

    Initial commit
    User Control - Content control

---
+++ C# Course/C_CourseCh12_Content_UserControl/C_CourseCh12_Content_UserControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace C_CourseCh12_Content_UserControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
