6fa77f81403b56b4567fa498147517e911fae50c
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2024-07-03 08:31:32 +0200

    Initial commit
    Data binding

---
+++ C# Course/C_Course_Ch12_DataBinding/C_Course_Ch12_DataBinding/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Course_Ch12_DataBinding.Data
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
