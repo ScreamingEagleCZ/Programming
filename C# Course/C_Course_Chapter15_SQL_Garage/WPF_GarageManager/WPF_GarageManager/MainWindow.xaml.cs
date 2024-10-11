3924f19b148cbe448995e3d77c66fbc3d8da5f38
ScreamingEagleCZ <jakub.mukarovsky@gmail.com>
2024-10-11 15:08:48 +0200

    Update of Garage Manager
    Added Buttons + partially functionality to them

--- C# Course/C_Course_Chapter15_SQL_Garage/WPF_GarageManager/WPF_GarageManager/MainWindow.xaml.cs
+++ C# Course/C_Course_Chapter15_SQL_Garage/WPF_GarageManager/WPF_GarageManager/MainWindow.xaml.cs
@@ -33,6 +33,7 @@ namespace WPF_GarageManager
             string connectionString = ConfigurationManager.ConnectionStrings["WPF_GarageManager.Properties.Settings.PanjutorialsDBConnectionString"].ConnectionString;
             sqlConnection = new SqlConnection(connectionString);
             ShowGarages();
+            ShowCars();
         }
 
         private void ShowGarages()
@@ -95,13 +96,226 @@ namespace WPF_GarageManager
             }
             catch (Exception ex)
             {
+                //MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void ShowCars()
+        {
+            try
+            {
+                string query = "select * from Car";
+                // the SqlDataAdapter can be imagined like an Interface to make Tables usable by C# Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)  // its opening and closing the sql connection as well
+                {
+                    DataTable carTable = new DataTable();
+
+                    sqlDataAdapter.Fill(carTable);
+
+                    // which Information of the Table in DataTable should be shown in our ListBox?
+                    listCars.DisplayMemberPath = "Name";
+                    // which Value should be delivered when an Item from our ListBox is selected?
+                    listCars.SelectedValuePath = "Id";
+                    // the reference to the Data the ListBox should populate
+                    listCars.ItemsSource = carTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.ToString());
             }
 
         }
+
         private void listGarages_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ShowAssociatedCars();
+            ShowSelectedGarageInTextBox();
+        }
+        private void listCars_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowSelectedCarInTextBox();
+        }
+        private void DeleteGarage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "delete from Garage where id = @GarageId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@GarageId", listGarages.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            } 
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.ToString());
+            } 
+            finally
+            {
+                sqlConnection.Close();
+                ShowGarages();
+            }
+
+
+        }
+        private void ShowSelectedGarageInTextBox()
+        {
+            try
+            {
+                string query = "select location from Garage where Id = @GarageId";
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // the SqlDataAdapter can be imagined like an Interface to make Tables usable by C# Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)  // its opening and closing the sql connection as well
+                {
+
+                    sqlCommand.Parameters.AddWithValue("@GarageId", listGarages.SelectedValue);
+
+                    DataTable garageDataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(garageDataTable);
+
+                    myTextBox.Text = garageDataTable.Rows[0]["Location"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+        private void ShowSelectedCarInTextBox()
+        {
+            try
+            {
+                string query = "select name from Car where Id = @CarId";
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // the SqlDataAdapter can be imagined like an Interface to make Tables usable by C# Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)  // its opening and closing the sql connection as well
+                {
+
+                    sqlCommand.Parameters.AddWithValue("@CarId", listCars.SelectedValue);
+
+                    DataTable garageDataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(garageDataTable);
+
+                    myTextBox.Text = garageDataTable.Rows[0]["Name"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+        private void AddGarage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "insert into Garage values (@Location)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Location", myTextBox.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowGarages();
+            }
+        }
+
+        private void UpdateGarage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveCar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddToGarage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "insert into GarageCar values (@GarageId, @CarId)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@GarageId", listGarages.SelectedValue);
+                sqlCommand.Parameters.AddWithValue("@CarId", listCars.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAssociatedCars();
+            }
+        }
+
+        private void UpdateCars_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddCarToList_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "insert into Car values (@Name)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Name", myTextBox.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowCars();
+            }
+        }
+
+        private void DeleteCarFromList_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "delete from Car where id = @CarId";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@CarId", listCars.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowCars();
+                ShowAssociatedCars();
+            }
         }
     }
 }
