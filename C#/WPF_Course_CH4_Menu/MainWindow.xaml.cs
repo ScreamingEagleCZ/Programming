8d2176cfecbc36c790825d5812b96c1e38b1151b
ScreamingEagleCZ <84529805+ScreamingEagleCZ@users.noreply.github.com>
2025-01-17 18:43:08 +0100

    Implemented status bar and menus

--- C#/WPF_Course_CH4_Menu/MainWindow.xaml.cs
+++ C#/WPF_Course_CH4_Menu/MainWindow.xaml.cs
@@ -16,9 +16,27 @@ namespace WPF_Course_CH4_Menu
     /// </summary>
     public partial class MainWindow : Window
     {
+        string text1 = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. " +
+                "Nulla quis diam. Phasellus et lorem id felis nonummy placerat. Nullam eget nisl. " +
+                "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus " +
+                "id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor " +
+                "repellendus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, " +
+                "per inceptos hymenaeos. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, " +
+                "velit. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. " +
+                "Fusce nibh. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. " +
+                "Nulla non lectus sed nisl molestie malesuada.";
+        string text2 = "Curabitur bibendum justo non orci. Maecenas sollicitudin. Nulla non lectus sed " +
+                "nisl molestie malesuada. Duis risus. Nullam justo enim, consectetuer nec, ullamcorper ac, " +
+                "vestibulum in, elit. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat " +
+                "ante, in pharetra metus odio a lectus. Maecenas sollicitudin. Fusce consectetuer risus a nunc. " +
+                "Integer pellentesque quam vel velit. Vivamus porttitor turpis ac leo. Duis aute irure dolor in " +
+                "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Phasellus et lorem " +
+                "id felis nonummy placerat. Nullam dapibus fermentum ipsum. Curabitur ligula sapien, pulvinar a " +
+                "vestibulum quis, facilisis vel sapien. Etiam posuere lacus quis dolor. Etiam commodo dui eget wisi.";
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -53,7 +71,7 @@ namespace WPF_Course_CH4_Menu
 
         private void Print_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(myTextBox.Text + " deleted!");
+            MessageBox.Show(myTextBox.Text + " printed!");
             myTextBox.Text = "";
         }
         private void Delete_Click(object sender, RoutedEventArgs e)
@@ -76,5 +94,87 @@ namespace WPF_Course_CH4_Menu
                 }
             }
         }
+
+        private void miItalic_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.FontStyle = FontStyles.Italic;
+        }
+
+        private void miNormal_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void miOblique_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.FontStyle = FontStyles.Oblique;
+        }
+
+        private void miBold_Checked(object sender, RoutedEventArgs e)
+        {
+            miLight.IsChecked = false;
+            myTextBox.FontWeight = FontWeights.Bold;
+        }
+
+        private void miBold_Unchecked(object sender, RoutedEventArgs e)
+        {
+            myTextBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void miLight_Checked(object sender, RoutedEventArgs e)
+        {
+            miBold.IsChecked = false;
+            myTextBox.FontWeight = FontWeights.Light;
+        }
+
+        private void miLight_Unchecked(object sender, RoutedEventArgs e)
+        {
+            myTextBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (miAltText.IsChecked)
+            {
+                myTextBox.Text = text2;
+            } else
+            {
+                myTextBox.Text = text1;
+            }
+        }
+        private void myTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = myTextBox.Text;
+            int length = text.Length;
+            sbFilling.Value = GetFilling(length);
+            if (sbFilling.Value < 20)
+            {
+                sbFilling.Foreground = Brushes.Yellow;
+            }
+            else if (sbFilling.Value < 80)
+            {
+                sbFilling.Foreground = Brushes.Orange;
+            }
+            else
+            {
+                sbFilling.Foreground = Brushes.Green;
+            }
+        }
+
+        private int GetFilling(int value)
+        {
+            if (value < 0)
+            {
+                return 0;
+            }
+            else if (value > 100)
+            {
+                return 100;
+            } else
+            {
+                return value;
+            }
+        }
+
     }
 }
